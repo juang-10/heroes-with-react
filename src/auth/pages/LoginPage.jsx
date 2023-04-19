4bbf4cddb1f49d450fa1c54286af810419ed9ddc
juang-10 <judagosa8520@gmail.com>
2023-04-19 17:10:12 -0500

    Feat: Navigate to the root path ('/') of the application, replacing the current history entry

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,25 @@
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  // Navegar a la ruta raíz ('/') de la aplicación, reemplazando la entrada actual en el historial
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    // Navegar a la ruta raíz ('/') de la aplicación, reemplazando la entrada actual en el historial
+    navigate('/', {
+      replace: true
+    })
+  }
   return (
-    <h1>LoginPage</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+      <button
+        className="btn btn-primary"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+    </div>
   )
 }
