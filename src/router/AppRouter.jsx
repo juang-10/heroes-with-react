f7fb554211d2b3160431a04f06dce865f7098a56
juang-10 <judagosa8520@gmail.com>
2023-04-19 17:04:16 -0500

    Add: comments

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,9 @@ import { LoginPage } from "../auth"
 export const AppRouter = () => {
   return (
     <Routes>
+      {/* Ruta para la página de inicio de sesión */}
       <Route path="/login" element={<LoginPage/>}/>
+      {/* Ruta comodín para todas las demás rutas */}
       <Route path="/*" element={<HeroesRoutes/>}/>
     </Routes>
   )
