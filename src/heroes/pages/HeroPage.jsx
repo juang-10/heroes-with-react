462f84a3f0ded4ac9f6c4bdf00d7c04f431f0e67
juang-10 <judagosa8520@gmail.com>
2023-04-20 10:56:36 -0500

    feat: display the name of the superhero

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,22 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
-export const Hero = () => {
+export const HeroPage = () => {
+  // Obtenemos el parámetro id de la URL con el hook useParams
+  const { id, ...rest } = useParams();
+
+  // Obtenemos el héroe correspondiente al id
+  const hero = getHeroById(id);
+
+  // Si el héroe no existe, redirigimos al usuario a la página de inicio de Marvel
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
+  // Si el héroe existe, mostramos su nombre de superhéroe en un elemento h1
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
+  );
+};
