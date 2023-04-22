a4dd7a3b9800193f221008327cfca0d03c2ad088
juang-10 <judagosa8520@gmail.com>
2023-04-22 14:54:58 -0500

    feat: Search for hero by name

---
+++ src/heroes/helpers/getHeroesByName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { heroes } from '../data/heroes';
+
+export const getHeroesByName = (name = '') => {
+  name.toLocaleLowerCase().trim();
+  if(name.length === 0) return [];
+  
+  return heroes.filter(
+    (hero) =>  hero.superhero.toLowerCase().includes(name)
+  )
+}
