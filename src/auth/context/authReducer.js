264edb36f91b21b465410c2751340cf7d63a152b
david <judagosa8520@gmail.com>
2024-03-29 13:08:06 -0500

    feat: add authReducer to handle login and logout actions

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+const initialState = {
+  logged: false,
+}
+
+export const authReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case types.login:
+      return {
+        ...state,
+        logged: true,
+        name: action.payload.name,
+      };
+
+    case types.logout:
+      return {
+        logged: false,
+      };
+    
+    default:
+      return state;
+  }
+
+}
