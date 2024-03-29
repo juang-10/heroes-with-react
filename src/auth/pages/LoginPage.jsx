e79f1009644b3a4e4846c05e8ce46029abbbefa5
david <judagosa8520@gmail.com>
2024-03-29 14:42:01 -0500

    feat: add login functionality to LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
+import { useContext } from "react";
 
 export const LoginPage = () => {
 
   const navigate = useNavigate();
 
+  const {login} = useContext(AuthContext);
+
   const onLogin = () => {
+    login('Juan Gonzalez')
     navigate('/', { replace: true });
   }
 
