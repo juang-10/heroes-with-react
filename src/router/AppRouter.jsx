18593adedc72649c5a16f2096bd40b13e6a35d45
david <judagosa8520@gmail.com>
2024-03-29 15:21:41 -0500

    feat: add PrivateRoute component to protect HeroRoutes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from '../auth'
 import { HeroRoutes } from "../heroes"
+import { PrivateRoute } from "./PrivateRoute"
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroRoutes />} />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroRoutes />
+          </PrivateRoute>
+        } />
       </Routes>
     </>
   )
