3f08928ac7586b5d54aead5b55d507481e5c16b1
david <judagosa8520@gmail.com>
2024-03-29 15:07:34 -0500

    feat: Add logout functionality to Navbar component

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -5,11 +5,12 @@ import { useContext } from 'react';
 
 export const Navbar = () => {
 
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
   
   const navigate = useNavigate();  
   
   const onLogout = () => {
+    logout();
     navigate('/login', { replace: true });
   }
 
